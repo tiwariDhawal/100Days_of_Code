e34861b48a7c9acfda8b44539bc3d9e8643c9137
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-06-08 20:06:56 +0530

    Day46

---
+++ Day46_Count-Negative-Numbers-in-a-Sorted-Matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int negativeNumbers = 0;
+        for(int i = 0;i< grid.size();i++){
+            for(int j = 0;j < grid[0].size();j++){
+                if(grid[i][j] < 0){
+                    negativeNumbers++;
+                }
+            }
+        }
+        return negativeNumbers;
+    }
+};
