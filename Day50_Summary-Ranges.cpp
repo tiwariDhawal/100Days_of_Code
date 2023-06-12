ec4339069126f5604c71f3b770e923032314e2da
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-06-12 21:04:28 +0530

    Day50

---
+++ Day50_Summary-Ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+
+        if(nums.size() < 1){
+            return ans;
+        }
+        for(int i= 0;i < nums.size();){
+            int low = i;
+            int high =i;
+            while(high + 1 < nums.size() && nums[high + 1] == nums[high] + 1){
+                high++;
+            }
+            if(high > low){
+                ans.push_back(to_string(nums[low]) + "->" + to_string(nums[high]));
+            }
+            else{
+                ans.push_back(to_string(nums[high]));
+            }
+            i = high + 1;
+        }
+        return ans;
+    }
+};
