34988ec007c4700ee99c0fdd24cca25a1a29e113
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-06-30 22:57:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort/merge-sort.cpp
+++ Medium/Merge Sort/merge-sort.cpp
@@ -22,61 +22,49 @@ class Solution
     void merge(int arr[], int l, int m, int r)
     {
          // Your code here
-         int len1 = m - l + 1;
-         int len2 = r - m;
-         
-         int *first = new int[len1];
-         int *second = new int[len2];
-         
-         int k = l;
-         for(int i = 0;i< len1;i++){
-             first[i] = arr[k++];
-         }
-         k = m + 1;
-         for(int i = 0;i< len2;i++){
-             second[i] = arr[k++];
-         }
-         
-         
+        int len1 = m - l + 1;
+        int len2 = r - m;
         
-         
-         //merge 2 sorted array
-         int i = 0;
-         int j = 0;
-         
-         int startingIndex = l;
-         while(i <len1 && j < len2){
-             if(first[i] < second[j]){
+        int *first = new int[len1];
+        int *second = new int[len2];
+        
+        int k = l;
+        for(int i = 0;i< len1;i++){
+            first[i] = arr[k++];
+        }
+        k = m + 1;
+        for(int i = 0;i< len2;i++){
+            second[i] = arr[k++];
+        }
+        
+        
+        int i = 0;
+        int j = 0;
+        int startingIndex = l;
+        while(i < len1 && j < len2){
+            if(first[i] < second[j]){
                  arr[startingIndex++] = first[i++];
              }
              else{
                  arr[startingIndex++] = second[j++];
              }
-         }
-         while(i < len1){
-             arr[startingIndex++] = first[i++];
-         }
-         while(j  < len2){
-             arr[startingIndex++] = second[j++];
-         }
-         
-         delete[] first;
-         delete[] second;
+        }
+        while(i < len1){
+            arr[startingIndex++] = first[i++];
+        }
+        while(j < len2){
+            arr[startingIndex++] = second[j++];
+        }
+        delete[] first;
+        delete[] second;
         
-
-         
+        
     }
     public:
     void mergeSort(int arr[], int l, int r)
     {
         //code here
-        //Step 1:Giving the values of mergesort 
-        //l = start
-        //r = end
-        //base case
-        if(l >= r){
-            return;
-        }
+        if(l >= r)return;
         int mid = l + (r-l)/2;
         
         
@@ -85,6 +73,7 @@ class Solution
         mergeSort(arr,l,mid);
         mergeSort(arr,mid+1,r);
         merge(arr,l,mid,r);
+        
     }
 };
 
