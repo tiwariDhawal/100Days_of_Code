f05afecc4021cc74b797e7b5f3ea473dfa1e440a
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-07-04 00:13:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Floor in BST/floor-in-bst.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int floor(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+
+int floor(Node* root, int x) {
+    // Code here
+    Node *ans = NULL;
+    if(root == NULL){
+        return -1;
+    }
+    Node *temp = root;
+    while(temp != NULL){
+        if(temp -> data == x){
+            return temp -> data;
+        }
+        if(temp ->data < x){
+            ans = temp;
+            temp = temp -> right;
+        }
+        else{
+            temp  = temp -> left;
+        }
+    }
+    if(ans!=NULL){
+        return ans -> data ;
+    }
+    return -1;
+}
