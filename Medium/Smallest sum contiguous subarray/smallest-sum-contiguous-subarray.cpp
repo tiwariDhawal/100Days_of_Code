ad1e933fc60c55b2540f06419518bb5c7478100e
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-07-06 13:47:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smallest sum contiguous subarray/smallest-sum-contiguous-subarray.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      //Code here
+      
+      int sum = 0;
+      int mini = INT_MAX;
+      for(int i = 0;i< a.size();i++){
+          sum = sum + a[i];
+          mini = min(sum,mini);
+          if(sum > 0){
+              sum = 0;
+          }
+      }
+      return mini;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
