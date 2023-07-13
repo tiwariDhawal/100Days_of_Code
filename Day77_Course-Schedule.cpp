b55edfe7b6cee65913995df23fbc3bb4f2bf7f4e
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-07-13 11:43:34 +0530

    Day77

---
+++ Day77_Course-Schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
+        vector<int> indegree(n, 0);
+        vector<int> ans;
+
+        for(auto x: prerequisites){
+            int u = x[0];
+            int v = x[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            auto t = q.front();
+            ans.push_back(t);
+            q.pop();
+
+            for(auto x: adj[t]){
+                indegree[x]--;
+                if(indegree[x] == 0){
+                    q.push(x);
+                }
+            }
+        }
+        return ans.size() == n;
+    }
+};
