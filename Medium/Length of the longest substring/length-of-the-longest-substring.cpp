70cf8b24a4e56b88b4966813425744efe3d6d4c7
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-07-15 02:37:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Length of the longest substring/length-of-the-longest-substring.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int longestUniqueSubsttr(string s){
+        //code
+        int i = 0;
+        int j = 0;
+        int size = s.length();
+        // map<char,int>mapping;
+        int mapping[26] = {0};
+        int maxi = 0;
+        while(j < size){
+            mapping[s[j] - 'a']++;
+            if(mapping[s[j] - 'a'] == 1){
+                maxi = max(maxi,j- i + 1);
+                j++;
+            }
+            else if(mapping[s[j] - 'a'] > 1){
+                while(mapping[s[j] - 'a'] > 1){
+                    mapping[s[i] - 'a']--;
+                    // if(mapping[s[i]] == 0){
+                    //     mapping.erase(s[i]);
+                    // }
+                    i++;
+                }
+                j++;
+            }
+        }
+        if(maxi != 0)return maxi;
+        return 0;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string str;
+		cin>>str;
+		Solution ob;
+		cout<<ob.longestUniqueSubsttr(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
