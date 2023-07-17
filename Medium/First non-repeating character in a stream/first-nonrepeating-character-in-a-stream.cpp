23276a6d871c2402be59f5152016624b024250dd
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-07-17 13:06:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First non-repeating character in a stream/first-nonrepeating-character-in-a-stream.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    unordered_map<char,int>count;
+		    string ans = "";
+		    queue<char>q;
+		    for(int i = 0;i<A.length();i++){
+		        char ch = A[i];
+		        count[ch]++;
+		        q.push(ch);
+		        while(!q.empty()){
+		            if(count[q.front()] > 1){
+		                q.pop();
+		            }
+		            else{
+		                ans.push_back(q.front());
+		                break;
+		            }
+		            
+		        }
+		        if(q.empty()){
+		            ans.push_back('#');
+		        }
+		    }
+		    return ans;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
