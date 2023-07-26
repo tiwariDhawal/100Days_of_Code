042826bdd2e50d49461ae2141ecf52e26280f239
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-07-26 15:50:36 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Insert in Middle of Linked List/insert-in-middle-of-linked-list.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// function to display the linked list
+void display(Node* head)
+{
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+Node* insertInMiddle(Node* head, int x);
+
+int main()
+{
+    int T, n, x;
+    cin>>T;
+    while(T--)
+    {
+        cin>> n >> x;
+        
+        struct Node *head = new Node(x);
+        struct Node *tail = head;
+        
+        for (int i=0; i<n-1; i++)
+        {
+            cin>>x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        
+        cin>> x;
+        head = insertInMiddle(head, x);
+        display(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to insert a node in the middle of the linked list.
+Node *middle(Node *head){
+    Node *slow = head;
+    Node *fast = head -> next;
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    return slow;
+}
+Node* insertInMiddle(Node* head, int x)
+{
+	// Code here
+	Node *middleElement = middle(head);
+	Node *newNode = new Node(x);
+	newNode -> next = middleElement -> next;
+	middleElement -> next = newNode;
+	return head;
+	
+	
+}
