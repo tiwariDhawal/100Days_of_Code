1538cb0785c13f74e9363f438dfb89f8c7673ad7
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-08-04 10:48:14 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -4,47 +4,49 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  private:
-   void bfs(int row , int col , vector<vector<int>>&vis,vector<vector<char>>& grid){
-       vis[row][col] = 1;
-       queue<pair<int,int>>q;
-       q.push({row,col});
-       int n = grid.size();
-       int m = grid[0].size();
-       while(!q.empty()){
-           int row = q.front().first;
-           int col = q.front().second;
-           q.pop();
-           
-           for(int delRow = -1;delRow <= 1;delRow++){
-               for(int delCol = -1;delCol <= 1;delCol++){
+  public:
+    // Function to find the number of islands.
+    void bfs(int row, int col, vector<vector<int>>&vis,vector<vector<char>>&grid){
+        int n= grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int delRow = -1;delRow <= 1;delRow++){
+                for(int delCol = -1;delCol <= 1;delCol++){
                    int nrow = row + delRow;
                    int ncol = col + delCol;
                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
                        vis[nrow][ncol] = 1;
                        q.push({nrow,ncol});
                    }
-               }
-           }
-       }
-   }
-  public:
-    // Function to find the number of islands.
+                }
+            }
+        }
+        
+        
+    }
     int numIslands(vector<vector<char>>& grid) {
         // Code here
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
         int count = 0;
-        for(int row = 0;row < n;row++){
-            for(int col = 0;col < m;col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i = 0;i< n;i++){
+            for(int j = 0;j < m;j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
                     count++;
-                    bfs(row,col,vis,grid);
+                    bfs(i,j,vis,grid);
                 }
             }
         }
         return count;
+        
     }
 };
 
