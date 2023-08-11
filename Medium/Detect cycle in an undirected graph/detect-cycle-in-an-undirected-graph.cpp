2ceaeb1e0089521b198c4a5fad54e59262e02018
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-08-11 18:20:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -4,48 +4,51 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-   private: 
-      bool detect(int src, vector<int> adj[], int vis[]) {
-          vis[src] = 1; 
-          // store <source node, parent node>
-          queue<pair<int,int>> q; 
-          q.push({src, -1}); 
-          // traverse until queue is not empty
-          while(!q.empty()) {
-              int node = q.front().first; 
-              int parent = q.front().second; 
-              q.pop(); 
-              
-              // go to all adjacent nodes
-              for(auto adjacentNode: adj[node]) {
-                  // if adjacent node is unvisited
-                  if(!vis[adjacentNode]) {
-                      vis[adjacentNode] = 1; 
-                      q.push({adjacentNode, node}); 
-                  }
-                  // if adjacent node is visited and is not it's own parent node
-                  else if(parent != adjacentNode) {
-                      // yes it is a cycle
-                      return true; 
-                  }
-              }
-          }
-                  // there's no cycle
-                  return false; 
-  }
   public:
     // Function to detect cycle in an undirected graph.
+    bool detect(int src,int V,vector<int>adj[],int vis[]){
+        vis[src] = 1;
+        queue<pair<int,int>>q;
+        q.push({src,-1});
+        
+        while(!q.empty()){
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            for(auto adjacentNode:adj[node]){
+                if(!vis[adjacentNode]){
+                    vis[adjacentNode] = 1;
+                    q.push({adjacentNode,node});
+                }
+                else if(parent != adjacentNode){
+                    return true;
+                    
+                }
+            }
+            
+        }
+        return false;
+    }
     bool isCycle(int V, vector<int> adj[]) {
-        // initialise them as unvisited 
+        // Code here
+        //step 1:making a vis array map adj list
+        //step 2:making a queue to check whether the element have its parent
+        //step 3:traverse it
         int vis[V] = {0};
-        for(int i = 0;i<V;i++) {
-            if(!vis[i]) {
-                if(detect(i, adj, vis)) return true; 
+        for(int i = 0;i< V;i++){
+            if(!vis[i]){
+                if(detect(i,V, adj, vis)) return true; 
             }
         }
-        return false; 
+        return false;
+        
+        
+        
+            
+        
+        
     }
-
 };
 
 //{ Driver Code Starts.
