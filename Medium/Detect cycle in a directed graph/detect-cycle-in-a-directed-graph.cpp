843e0e877650084ead32c1346e4b02807d7a4bef
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-08-13 14:55:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -23,16 +23,44 @@ class Solution {
     }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<int>vis(V,0);
-        vector<int>pathVis(V,0);
-        for(int i = 0;i< V;i++){
-            if(!vis[i]){
-                if((dfs(i,adj,vis,pathVis)) == true){
-                    return true;
-                }
-            }
-        }
-        return false;
+        // vector<int>vis(V,0);
+        // vector<int>pathVis(V,0);
+        // for(int i = 0;i< V;i++){
+        //     if(!vis[i]){
+        //         if((dfs(i,adj,vis,pathVis)) == true){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        // code here
+	    int indegree[V] = {0};
+	    for(int i = 0;i< V;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int>q;
+	    for(int i = 0;i< V;i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    int count = 0;
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        count++;
+	        
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it] == 0){
+	                q.push(it);
+	            }
+	        }
+	    }
+	    if(count == V)return false;
+	    return true;
     }
 };
 
