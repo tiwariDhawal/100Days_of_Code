616b8c3513d64e2f52cc555924b9cb75a65214d2
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-09-05 13:05:17 +0530

    Updated Solution - GfG to GitHub

--- Easy/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
+++ Easy/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,30 +29,48 @@ int main() {
 // } Driver Code Ends
 
 
+
+
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
         vector<long long> ans;
         queue<long long> q;
-        for(int i = 0;i< K-1;i++){
-            if(A[i] < 0){
-                q.push(A[i]);
+        // for(int i = 0;i< K-1;i++){
+        //     if(A[i] < 0){
+        //         q.push(A[i]);
+        //     }
+            
+        // }
+        int i = 0;
+        int j = 0;
+        
+        while (j < N) {
+            if (j - i + 1 < K) {
+                if (A[j] < 0) {
+                    q.push(A[j]);
+                }
+                j++;
             }
             
-        }
-        for(int i= K - 1;i< N;i++){
-            if(A[i] < 0){
-                q.push(A[i]);
-            }
-            if(!q.empty()){
-                ans.push_back(q.front());
-                if(q.front() == A[i - K + 1]){
-                    q.pop();
+            if (j - i + 1 == K) {
+                if (A[j] < 0) {
+                    q.push(A[j]);
                 }
-            }
-            else{
-                ans.push_back(0);
+                
+                if (!q.empty()) {
+                    ans.push_back(q.front());
+                    if (q.front() == A[i]) {
+                        q.pop();
+                    }
+                } else {
+                    ans.push_back(0);
+                }
+                
+                i++;
+                j++;
             }
         }
-        return ans;
+    
+    return ans;
                                                  
  }
