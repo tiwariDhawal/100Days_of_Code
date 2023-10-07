65a18f2e46a3b787935b4b37c7de9e9c93d88357
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-10-07 21:20:40 +0530

    Updated Solution - GfG to GitHub

--- Easy/Pattern 22/pattern-22.cpp
+++ Easy/Pattern 22/pattern-22.cpp
@@ -8,14 +8,13 @@ class Solution {
   public:
     void printSquare(int n) {
         // code here
-        for(int i = 0;i< 2*n - 1;i++){
-            for(int j = 0;j < 2*n - 1;j++){
+        for(int i = 0;i< 2 * n - 1;i++){
+            for(int j = 0;j < 2 * n - 1;j++){
                 int top = i;
                 int left = j;
-                int right = (2 * n - 2) - j;
-                int down = (2 * n - 2) - i;
-                cout<< (n - min(min(top,down),min(left,right))) << " ";
-                
+                int bottom = ((2 * n) - 2) - i;
+                int right = ((2 * n) - 2) - j;
+                cout<<(n - min(min(top,bottom),min(left,right))) << " ";
             }
             cout<<endl;
         }
