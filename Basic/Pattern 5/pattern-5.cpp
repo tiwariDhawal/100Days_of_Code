5dc2ffbda09d87de6217e4392416f73767af933f
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-10-07 20:57:37 +0530

    Updated Solution - GfG to GitHub

--- Basic/Pattern 5/pattern-5.cpp
+++ Basic/Pattern 5/pattern-5.cpp
@@ -9,11 +9,11 @@ public:
 	
 	void printTriangle(int n) {
 	    // code here
-	    for(int i = 0;i< n;i++){
-	        for(int j = 0;j <= n - i - 1;j++){
-	            cout<<"*" <<" ";
-	        }
-	        cout<<endl;
+	    for(int i = 1;i<= n;i++){
+        for(int j = 1;j <= n - i + 1;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
 	    }
 	}
 };
