672f382ae90e3fafcff0fbf4474d921f4e881853
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-10-08 21:18:30 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rotten Oranges/rotten-oranges.cpp
+++ Medium/Rotten Oranges/rotten-oranges.cpp
@@ -11,48 +11,53 @@ class Solution
         // Code here
         int n = grid.size();
         int m = grid[0].size();
-        int countFresh = 0;
-        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
-        int timer = 0;
-        for(int i = 0;i<n;i++){
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        //Step1:Making the copy of the grid which is visited or not 
+        // a> Taking the count of fresh oranges 
+        int countFresh = 0;
+        for(int i = 0;i< n;i++){
             for(int j = 0;j< m;j++){
-                
                 if(grid[i][j] == 2){
-                    q.push({{i,j},0});
                     vis[i][j] = 2;
-                    
+                    q.push({{i,j},0});
                 }
-                else{
+                else if(grid[i][j] == 0){
                     vis[i][j] = 0;
                 }
-                if(grid[i][j] == 1 ){
+                if(grid[i][j] == 1){
                     countFresh++;
                 }
+                
             }
         }
+        //Step2:Now checking the neighbours for all the directions 
+        int delRow[] = {-1,0,+1,0};
+        int delCol[] = {0,+1,0,-1};
+        //b>Checking for the minimum time to rott all the oranges 
+        int timer = 0;
         int count = 0;
-   
-        int delR[] = {-1,0,+1,0};
-        int delC[] = {0,+1,0,-1};
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
             int tm = q.front().second;
             q.pop();
-            timer = max(tm,timer);
             
+            timer = max(timer,tm);
             for(int i = 0;i< 4;i++){
-                int nrow = row + delR[i];
-                int ncol = col + delC[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                    q.push({{nrow,ncol},tm + 1});
-                    vis[nrow][ncol] = 2;
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                    q.push({{nrow,ncol},tm+1});
+                    vis[nrow][ncol]= 1;
                     count++;
                 }
             }
         }
-        if(count != countFresh)return -1;
+        if(count != countFresh){
+            return -1;
+        }
         return timer;
         
         
