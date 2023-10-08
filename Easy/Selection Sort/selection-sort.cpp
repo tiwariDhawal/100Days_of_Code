7185fa8252e538def073d4deb45d673b208b8d58
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2023-10-08 20:37:50 +0530

    Updated Solution - GfG to GitHub

--- Easy/Selection Sort/selection-sort.cpp
+++ Easy/Selection Sort/selection-sort.cpp
@@ -21,15 +21,15 @@ class Solution
     void selectionSort(int arr[], int n)
     {
        //code here
-        for(int i = 0;i < n-1;i++){
-            int minIndex = i;
-            for(int j = i + 1;j < n;j++){
-                if(arr[j] < arr[minIndex]){
-                    minIndex = j;
-                }
-            }
-            swap(arr[i],arr[minIndex]);
-        }
+       for(int i = 0;i< n;i++){
+           int minIndex = i;
+           for(int j = i + 1;j < n;j++){
+               if(arr[j] < arr[minIndex]){
+                   minIndex = j;
+               }
+           }
+           swap(arr[i],arr[minIndex]);
+       }
     }
 };
 
